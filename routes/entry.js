23e9e030e5f53fee3df89ce3708da0c217cde571
Arshhasan <arshhasan2016@gmail.com>
2025-06-08 16:42:22 +0530

    CORS issue

--- routes/entry.js
+++ routes/entry.js
@@ -1,17 +1,17 @@
-// routes/entry.js
 const express = require('express');
 const router = express.Router();
 
-router.options('/submit', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  return res.sendStatus(204); // Preflight success
-});
-
 const { submitEntry } = require('../controllers/entryController');
 
-// Submit a new crush entry
+// Allow preflight for any route in this file
+router.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  return res.sendStatus(204); // No Content
+});
+
+// Actual endpoint
 router.post('/submit', submitEntry);
 
 module.exports = router;
