afd2abebb96f85cae725931ed1815b9b762d3d41
Arshhasan <arshhasan2016@gmail.com>
2025-06-08 16:39:50 +0530

    CORS issue

--- routes/entry.js
+++ routes/entry.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.options('/submit', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  return res.sendStatus(204); // Preflight success
+});
+
 const { submitEntry } = require('../controllers/entryController');
 
 // Submit a new crush entry
